f8045394dca11f7368d0a9c7ab2ea2617db77bae
Martijn Bulinga <94762243+skorpioo@users.noreply.github.com>
2022-12-22 21:22:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const rangeSlider = document.querySelector("[data-slider]");
+const pageViews = document.querySelector("[data-pageviews]");
+const price = document.querySelector("[data-price]");
+const fullPrice = document.querySelector("[data-fullprice]");
+const time = document.querySelector("[data-time]");
+const toggle = document.querySelector("[data-toggle]");
+
+var prices = [8, 12, 16, 24, 36];
+var views = ["10K", "50K", "100K", "500K", "1M"];
+
+rangeSlider.oninput = function () {
+	pageViews.innerHTML = views[this.value];
+	if (toggle.checked) {
+		price.innerHTML = "$" + prices[this.value] * 12 * 0.75 + ",-";
+		fullPrice.innerHTML =
+			"You save: $" + (prices[this.value] * 12 - prices[this.value] * 12 * 0.75) + ",-";
+
+		time.innerHTML = "/ year";
+	} else {
+		price.innerHTML = "$" + prices[this.value] + ",-";
+		fullPrice.innerHTML = "";
+		fullPrice.style.padding = "0";
+		time.innerHTML = "/ month";
+	}
+
+	toggle.addEventListener("change", e => {
+		if (e.target.checked) {
+			price.innerHTML = "$" + prices[this.value] * 12 * 0.75 + ",-";
+			fullPrice.innerHTML =
+				"You save: $" + (prices[this.value] * 12 - prices[this.value] * 12 * 0.75) + ",-";
+			time.innerHTML = "/ year";
+		} else {
+			price.innerHTML = "$" + prices[this.value] + ",-";
+			fullPrice.innerHTML = "";
+			fullPrice.style.padding = "0";
+			time.innerHTML = "/ month";
+		}
+	});
+};
+rangeSlider.oninput();
+
+rangeSlider.addEventListener("mousemove", function () {
+	var x = rangeSlider.value * 25;
+	var color =
+		"linear-gradient(90deg, var(--clr-accent-soft)" + x + "%, var(--clr-empty-slider-bar)" + x + "%)";
+	rangeSlider.style.background = color;
+});
